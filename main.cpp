7815b018784e656c733c690d0673d4107edad1af
johanfortus <fortusjohan@gmail.com>
2024-08-07 13:29:02 -0400

    window / board setup values

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <fstream>
+#include <sstream>
 #include <vector>
 #include <string>
 #include <map>
@@ -7,23 +9,46 @@
 using namespace std;
 
 int main(){
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    ifstream inFile("config.cfg");
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    string lineFromFile;
+
+    int width;
+    inFile >> width;
+    width = width * 32;
+    cout << "Width: " << width << endl;
+
+    int height;
+    inFile >> height;
+    height = (height * 32) + 100;
+    cout << "Height: " << height << endl;
+
+    int mineCount;
+    inFile >> mineCount;
+    mineCount = mineCount * 16;
+    cout << "Mine Count: " << mineCount << endl;
+
+    inFile.close();
+
+
+//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+//
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        window.clear();
+//        window.draw(shape);
+//        window.display();
+//    }
 
     return 0;
 }
