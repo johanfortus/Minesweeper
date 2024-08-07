958082baa1818752bccddcf82a3548320cdf86bd
johanfortus <fortusjohan@gmail.com>
2024-08-07 12:43:56 -0400

    added files

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <cmath>
+using namespace std;
+
+int main(){
+    cout << "due tomorrow yay" << endl;
+    return 0;
+}
